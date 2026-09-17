fb7cc236fc767ba1ea299c5f8c1c6367f39c6153
Awaneesh0 <awaneesh209@gmail.com>
2026-09-17 21:53:33 +0530

    Add files via upload

---
+++ Solution_232.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+    
+    // Push element x to the back of queue.
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    // Removes the element from in front of queue and returns that element.
+    public int pop() {
+        shiftStacks();
+        return outStack.pop();
+    }
+    
+    // Get the front element.
+    public int peek() {
+        shiftStacks();
+        return outStack.peek();
+    }
+    
+    // Returns whether the queue is empty.
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+    
+    // Helper method to move elements from inStack to outStack
+    private void shiftStacks() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
